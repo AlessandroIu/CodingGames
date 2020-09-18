d397ee3fea07562b5769883c2cfa6808d3815c34
AlessandroIu <alessandroiudicone@gmail.com>
2020-09-18 08:41:10 -0700

    Correction of OrderSystemInput; Added task descriptions for some other games

--- src/toptalTests/task3/SplitStringOfAAndBIntoThreeSeparateParts.java
+++ src/toptalTests/task3/SplitStringOfAAndBIntoThreeSeparateParts.java
@@ -1,3 +1,5 @@
+package toptalTests.task3;
+
 public class SplitStringOfAAndBIntoThreeSeparateParts {
 
     public static void main(String[] args) {
---
+++ src/utils/OrderSystemInput.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class OrderSystemInput {
+
+    public static void main(String... argv ){
+
+        Scanner in = new Scanner(System.in);
+        List<List<String>> lines = new ArrayList<>();
+        while(in.hasNextLine()) {
+            String line = in.nextLine().trim();
+            lines.add(Arrays.asList(line.split(" ")));
+        }
+        System.out.print(lines);
+    }
+}
